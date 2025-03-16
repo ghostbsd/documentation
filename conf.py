a5856501a49cd563ceee489fa85241d5627b9866
ericbsd <ericturgeon.bsd@gmail.com>
2025-03-15 23:38:42 -0300

    Adding Edit on GitHub

--- conf.py
+++ conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -57,6 +57,14 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+
+# Display "Edit on GitHub"
+html_context = {
+  'display_github': True,
+  'github_user': 'ghostbsd',
+  'github_repo': 'documentation',
+  'github_version': 'main/',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
